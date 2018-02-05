fb3ff7d9e45da3717112a791c425464aa99deae8
Nicole <niste7@morgan.edu>
2018-02-04 20:35:20 -0500

    final commit

---
+++ flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Nicole  Steele on 2/4/18.
+//  Copyright © 2018 Nicole  Steele. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
